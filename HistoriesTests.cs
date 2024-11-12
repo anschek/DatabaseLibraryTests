a19d26824490f6f5741aae20a3f56f5305ec4a27
anschek <anschek@yandex.ru>
2024-11-12 14:43:39 +0300

    test: check method of getting groupings by 7 statuses

--- HistoriesTests.cs
+++ HistoriesTests.cs
@@ -1,3 +1,5 @@
+using DatabaseLibrary.Entities.DTOs;
+using DatabaseLibrary.Entities.ProcurementProperties;
 using System.Diagnostics;
 
 namespace Tests
@@ -52,6 +54,46 @@ namespace Tests
 
                 for (int j = 0; j < secondGroups[i].TenderCount; ++j)
                     Assert.AreEqual(firstGroups[i].Item5[j].Id, secondGroups[i].Procurements[j].Id);
+            }
+        }
+
+        [TestMethod]
+        public async Task GetHistoriesGroupByProcurementState_PassAllStates_Returns7CorrectStatesAndgroups()
+        {
+            // Arrange
+            var histories = await DatabaseLibrary.Controllers.GET.Histories.Many();
+            var targetStatuses = new List<string> { "Выигран 2ч", "Отправлен", "Оформлен", "Посчитан", "Новый", "Отбой", "Отклонен" };
+            List<List<Tuple<int, int, decimal, int, List<Procurement>>>?> firstAllGroups = new();
+
+            // Act
+            foreach (string status in targetStatuses)
+                firstAllGroups.Add(DatabaseLibrary.Queries.GET.View.HistoryGroupBy(status, histories).Item1);
+
+            var secondAllGroups = await DatabaseLibrary.Controllers.GET.Histories.GroupByProcurementState(histories);
+
+            // Assert
+            Assert.AreEqual(targetStatuses.Count, secondAllGroups.Count);
+            Assert.AreEqual(firstAllGroups.Count, secondAllGroups.Count);
+
+            for (int groupsByState = 0; groupsByState < targetStatuses.Count; ++groupsByState)
+            {
+                var firstStateGroups = firstAllGroups[groupsByState];
+                var secondStateGroups = secondAllGroups[targetStatuses[groupsByState]]; // with status from list statuses by index
+
+                Assert.AreEqual(firstStateGroups.Count, secondStateGroups.Count);
+                for (int innerGroup = 0; innerGroup < firstAllGroups[groupsByState].Count; ++innerGroup)
+                {
+                    var firstGroup = firstStateGroups[innerGroup];
+                    var secondGroup = secondStateGroups[innerGroup];
+                    Assert.AreEqual(firstGroup.Item1, secondGroup.Year);
+                    Assert.AreEqual(firstGroup.Item2, secondGroup.Month);
+                    Assert.AreEqual(firstGroup.Item3, secondGroup.TotalAmount);
+                    Assert.AreEqual(firstGroup.Item4, secondGroup.TenderCount);
+
+                    for (int procurementsInGroup = 0; procurementsInGroup < secondGroup.TenderCount; ++procurementsInGroup)
+                        Assert.AreEqual(firstGroup.Item5[procurementsInGroup].Id, secondGroup.Procurements[procurementsInGroup].Id);
+                }
+                Debug.WriteLine($"group with state '{targetStatuses[groupsByState]}' complete successufully");
             }
         }
     }
