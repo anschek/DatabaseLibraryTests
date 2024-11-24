7e7b003600cb940cd56dfb62bd7c4fdcb2e47c59
anschek <anschek@yandex.ru>
2024-11-24 19:14:43 +0300

    test: check method of getting distribution of procurement states

---
+++ ProcurementsStatesTests.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tests
+{
+    [TestClass]
+    public class ProcurementsStatesTests
+    {
+        [TestMethod]
+        public async Task GetProcurementsStatesManyByEmployeePosition_PassAllEmployeePositions_ReturnsSameAsDistributionOfProcurementStates()
+        {
+            // Arrange
+            var employeePositions = new string[]{
+                 "Администратор","Руководитель отдела расчетов","Заместитель руководителя отдела расчетов", "Специалист отдела расчетов",
+                    "Руководитель тендерного отдела","Заместитель руководителя тендреного отдела","Специалист тендерного отдела",
+                    "Специалист по работе с электронными площадками","Руководитель отдела закупки","Заместитель руководителя отдела закупок",
+                    "Специалист закупки", "Руководитель отдела производства","Заместитель руководителя отдела производства","Специалист по производству",
+                    "Юрист"
+            };
+
+            foreach (var position in employeePositions)
+            {
+                // Act
+                var expected = DatabaseLibrary.Queries.GET.View.DistributionOfProcurementStates(position);
+                var real = await DatabaseLibrary.Controllers.GET.ProcurementStates.ManyByEmployeePosition(position);
+                // Assert
+                Assert.AreEqual(expected.Count, real.Count);
+                for(int i=0; i<expected.Count; i++) Assert.AreEqual(expected[i].Id, real[i].Id);
+            }
+        }
+    }
+}
